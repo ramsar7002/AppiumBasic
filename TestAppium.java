3f07973716d9012ab5b0027f30c28ddd64754aed
Ram Sarfian <39989110+ramsar7002@users.noreply.github.com>
2021-07-12 11:54:10 +0300

    Update TestAppium.java

--- TestAppium.java
+++ TestAppium.java
@@ -4,18 +4,21 @@ import org.testng.annotations.AfterTest;
 import org.testng.annotations.Test;
 
 import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.MobileElement;
 import io.appium.java_client.android.AndroidDriver;
 
 import java.io.File;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.concurrent.TimeUnit;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.remote.DesiredCapabilities;
 
-public class TestAppium {
+public class tetete {
 	AppiumDriver driver;
 
 	@Test
@@ -26,21 +29,44 @@ public class TestAppium {
 		capabilities.setCapability("platformVersion", "9");
 		capabilities.setCapability("platformName", "Android");
 		
-		File file = new File("C:\\Users\\user\\eclipse-workspace\\teststs\\src\\apk\\Indeed Job Search_v72.0_apkpure.com.apk");
-		capabilities.setCapability("app", file.getAbsolutePath());
+		File file = new File("C:\\Users\\user\\eclipse-workspace\\teststs\\src\\apk\\beehd.android_4.7.0.apk");
+		//capabilities.setCapability("app", file.getAbsolutePath());
 
 
 		
 		try {
-			URL url = new URL("http://10.72.100.80:4723/wd/hub");
+			URL url = new URL("http://localhost:4723/wd/hub");
 	        
-			 driver = new AppiumDriver(url, capabilities);
+			 driver = new AppiumDriver<MobileElement>(url, capabilities);
 		} catch (MalformedURLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 
 
+	}
+	
+	public void makeACall() {
+		MobileElement el = (MobileElement) driver.findElementByAccessibilityId("Dial");
+		el.click();
+		driver.manage().timeouts().implicitlyWait(7, TimeUnit.SECONDS);
+
+
+		el = (MobileElement) driver.findElementByAccessibilityId("Transmitters");
+		el.click();
+		driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+		
+		el = (MobileElement) driver.findElementByAccessibilityId("More options");
+		el.click();
+		driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+		
+		el = (MobileElement) driver.findElementByLinkText("Show Video");
+		driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+
+
+		
+		el = (MobileElement) driver.findElementById("com.radvision.beehd:id/incall_contact_item_uri\r\n"+ "");
+		el.click();
 	}
 	
 	@AfterTest
@@ -51,9 +77,10 @@ public class TestAppium {
 		System.setProperty("webdriver.gecko.driver","C:\\Users\\user\\Desktop\\geckodriver.exe");
 //		WebDriver driver = new FirefoxDriver();
 //		driver.get("http://google.com");
-		TestAppium test = new tetete();
-		test.testAppium();
-		test.afterTest();
+		tetete tetete = new tetete();
+		tetete.testAppium();
+		//tetete.afterTest();
+		tetete.makeACall();
 	
 	}
 
